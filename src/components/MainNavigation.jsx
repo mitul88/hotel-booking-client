5c61caadc25641cbfba155c94a7819f9b5e21fb4
shahed <shahed.rahman88@gmail.com>
2023-10-10 22:35:44 +0600

    cart dropdown added with mui

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,14 +1,25 @@
-import { Badge, Button } from '@mui/material'
+import { Badge, Button, Menu, MenuItem } from '@mui/material'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Cart from './shared/Cart';
 
 const MainNavigation = () => {
   const [showCart, setShowCart] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
   const toggleCart = () => {
     setShowCart(!showCart)
   }
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <header className='relative w-screen h-[80px] flex flex-row justify-between px-20 py-5 bg-blue-900'>
       <Link to=""  className='flex items-center'>
@@ -19,8 +30,28 @@ const MainNavigation = () => {
       <div className='flex justify-between min-w-[400px]'>
         <nav>
           <Badge badgeContent={2} color='error'>
-            <Button variant='outlined' onClick={toggleCart} className='text-white'>Your bookings</Button>
+            <Button 
+              variant='outlined' 
+              className='text-white'
+              id="cart-button"
+              aria-controls={open ? 'cart-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}  
+            >Your bookings</Button>
+            {/* <Button variant='outlined' onClick={toggleCart} className='text-white'>Your bookings</Button> */}
           </Badge>
+          <Menu
+            id="cart-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'cart-button',
+            }}
+          >
+            <Cart onClose={handleClose} />
+          </Menu>
         </nav>
         <nav>
           <Button
